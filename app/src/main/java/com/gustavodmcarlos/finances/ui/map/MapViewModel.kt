598f6c99a624aec3044c5deac1adf486293bed99
Gustavo de Medeiros <gustavo.10.dmc@gmail.com>
2021-11-28 17:14:43 -0300

    Add charts to Statistics Screen

--- app/src/main/java/com/gustavodmcarlos/finances/ui/map/MapViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.gustavodmcarlos.finances.ui.map
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class MapViewModel : ViewModel() {
-
-}
